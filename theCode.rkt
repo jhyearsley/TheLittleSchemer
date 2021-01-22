679bb7d005df5104dc1f995c720d2b478f097bae
John Yearsley <yearsj@uw.edu>
2021-01-21 21:49:07 -0800

    Finished writing intersectall and added example

--- theCode.rkt
+++ theCode.rkt
@@ -472,3 +472,70 @@
        (cons (car lat)
              (makeset
               (rember* (car lat) (cdr lat))))))))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      ((and (member? (car set1) set2)
+            (subset? (cdr set1) set2)))
+      (else #f))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and (subset? set1 set2)
+         (subset? set2 set1))))
+      
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else
+       (or (member? (car set1) set2)
+           (intersect? (cdr set1) set2))))))
+       
+       
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) '())
+      ((member? (car set1) set2)
+       (cons (car set1)
+             (intersect (cdr set1) set2)))
+      (else (intersect (cdr set1) set2)))))
+
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2)
+       (union (cdr set1) set2))
+      (else (cons (car set1)
+                  (union (cdr set1) set2))))))
+
+(define difference
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) '())
+      ((member? (car set1) set2)
+       (difference (cdr set1) set2))
+      (else (cons (car set1)
+                  (difference (cdr set1) set2))))))
+
+
+    
+                     
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else (intersect (car l-set)
+                       (intersectall (cdr l-set)))))))
+       
+       
+;(intersectall '((1 2 3) (1 2) (1)))
+;(intersect '(1 2 3) (intersectall '((1 2) (1)))))
+;(intersect '(1 2 3) (intersect '(1 2) (intersectall '((1)))))
+;(intersect '(1 2 3) (intersect '(1 2) '(1)))
+;(intersect '(1 2 3) '(1))
+;'(1)                
